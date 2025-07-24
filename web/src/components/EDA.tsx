2b15e1c8220a5f4da319df64e9949298476c7e9d
JayGhz <jaykansas7@gmail.com>
2025-07-23 23:29:05 -0500

    style(components): adjust padding and margins for responsive layouts
    Update padding and margin values in Dataset and EDA components to improve responsiveness across different screen sizes. The changes include:
    - Added sm:p-20 and md:p-24 breakpoints in Dataset
    - Adjusted margins and gaps in EDA for better spacing consistency

--- web/src/components/EDA.tsx
+++ web/src/components/EDA.tsx
@@ -10,7 +10,7 @@ export default function EDA() {
     return (
         <div className="md:p-[0em] mx-8 lg:m-20 mt-24 xl:mx-64 lg:mb-28 mb-32 md:mt-4">
 
-            <div className="grid grid-cols-1 grid-rows-3 lg:grid-cols-2 lg:grid-rows-2 gap-8 lg:gap-4">
+            <div className="grid grid-cols-1 grid-rows-3 lg:grid-cols-2 lg:grid-rows-2 mt-20 xl:m-0 gap-8 lg:gap-4">
 
 
                 <div className="border border-zinc-300 lg:col-span-2 dark:border-zinc-200/20 rounded-xl p-6">
@@ -37,9 +37,9 @@ export default function EDA() {
                 </div>
             </div>
 
-            <div className="lg:my-12 my-9" />
+            <div className="lg:my-12 my-8" />
 
-            <div className="grid grid-cols-1 grid-rows-3 lg:grid-cols-2 lg:grid-rows-2 gap-4">
+            <div className="grid grid-cols-1 grid-rows-3 lg:grid-cols-2 lg:grid-rows-2 gap-8 lg:gap-4">
                 <div className="border border-zinc-300 lg:col-span-2 dark:border-zinc-200/20 rounded-xl pt-6 pb-11">
                     <div className="text-sm mb-6 dark:text-zinc-200/90 font-semibold text-center">
                         Evolución del Score de Riesgo
